2c664e5ec3361fa657129003368e86b506eeb74c
CFM <christianfrans29m@gmail.com>
2021-01-18 11:14:49 +0700

    First Commit

---
+++ TravelNow.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package travelnow;
+
+import travelnow.Frame.SplashScreen;
+
+/**
+ *
+ * @author user
+ */
+public class TravelNow {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		SplashScreen loading = new SplashScreen();
+		loading.setVisible(true);
+	}
+}
